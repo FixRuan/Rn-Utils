88cd2c8f193808998f7cb4a2ac4fa1a90e3ef1c2
fixruan <ruangoio01@gmail.com>
2023-01-02 20:51:48 -0300

    useKeyPressEvent

--- reactUse/src/App.tsx
+++ reactUse/src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import {useKeyPressEvent} from "react-use";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count => count + 1);
+  const decrement = () => setCount(count => count - 1);
+
+  useKeyPressEvent("e", increment);
+  useKeyPressEvent("r", decrement);
+
   return (
     <div>
-      <h1>React Use</h1>
+      <h1>{count}</h1>
     </div>
   );
 }
