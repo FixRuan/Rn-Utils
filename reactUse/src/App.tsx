7f8e50633a068adba6b728b2bce5104c7a9ca229
fixruan <ruangoio01@gmail.com>
2023-01-02 21:07:24 -0300

    Use Scroll hook

--- reactUse/src/App.tsx
+++ reactUse/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {useKeyPressEvent} from "react-use";
+import {useKeyPressEvent, useWindowScroll, useWindowSize} from "react-use";
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -9,9 +9,15 @@ export default function App() {
   useKeyPressEvent("e", increment);
   useKeyPressEvent("r", decrement);
 
+  const { x, y } = useWindowScroll();
+  const { width, height } = useWindowSize();
+
   return (
     <div>
       <h1>{count}</h1>
+      <h3>x: {x}</h3>
+      <h3>y: {y}</h3>
+      <h3>Window size: {`${width} x ${height}`}</h3>     
     </div>
   );
 }
