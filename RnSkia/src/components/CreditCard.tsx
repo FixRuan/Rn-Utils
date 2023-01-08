297e47da58ded5254c4953a3a0f6c5d2fd726d98
fixruan <ruangoio01@gmail.com>
2023-01-08 11:54:22 -0300

    Credit Card Style with Canvas

--- RnSkia/src/components/CreditCard.tsx
+++ RnSkia/src/components/CreditCard.tsx
@@ -1,28 +1,92 @@
 import React from "react";
+import {ActivityIndicator, Dimensions, TouchableOpacity} from "react-native";
+
+import Lato from "../assets/fonts/Lato.ttf";
+
 import { 
   Canvas, 
   RoundedRect, 
   LinearGradient, 
   vec, 
-  Circle 
+  Circle,
+  Group,
+  useFont,
+  Text,
+  TextPath,
+  Skia,
+  Blur,
 } from "@shopify/react-native-skia";
-import {Dimensions} from "react-native";
 
 const width = Dimensions.get("window").width - 24;
 const height = 200;
 
 export function CreditCard(){
-  return(
-    <Canvas style={{ height, width }}>
-      <RoundedRect x={0} y={0} width={width} height={height} r={10}>
-        <LinearGradient 
-          start={vec(0,0)}
-          end={vec(256,256)}
-          colors={["#5b9a93", "#2e645f", "#40534d", "#5b9a93"]}
-        />
-      </RoundedRect>
+  const font = useFont(Lato, 16);
+  const circle = Skia.Path.Make();
+  circle.addCircle(30, -10, 50);
 
-      <Circle cx={width - 60} cy={40} r={20} color="#d6a485" />
-    </Canvas>
+  if(font === null){
+    return <ActivityIndicator size={24}/>;
+  }
+
+  return(
+    <TouchableOpacity activeOpacity={0.8}>
+      <Canvas style={{ height, width }}>
+        <RoundedRect x={0} y={0} width={width} height={height} r={10}>
+          <LinearGradient 
+            start={vec(0,0)}
+            end={vec(256,256)}
+            colors={["#5b9a93", "#2e645f", "#40534d", "#5b9a93"]}
+          />
+        </RoundedRect>
+
+        <Group blendMode="overlay">
+          <Circle cx={width - 60} cy={40} r={20} color="#d6a485" />
+          <Circle cx={width - 40} cy={40} r={20} color="#98504b" />
+        </Group>
+
+        <Text 
+          x={20}
+          y={50}
+          font={font}
+          color="#FFFFFF"
+          text="Operadora do cartão"
+        />
+
+        <Text 
+          x={20}
+          y={height - 90}
+          font={font}
+          color="#FFFFFF"
+          text="5432 2341 2356 8921"
+        />
+
+        <Text 
+          x={20}
+          y={height - 30}
+          font={font}
+          color="#FFFFFF"
+          text="12/2025"
+        />
+
+        <Text 
+          x={width - 60}
+          y={height - 30}
+          font={font}
+          color="#FFFFFF"
+          text="000"
+        />
+
+        <TextPath 
+          text="Cartão de crédito virtual"
+          font={font}
+          path={circle}
+          color="#000000"
+          opacity={0.3}
+        >
+          <Blur blur={1}/>
+        </TextPath>
+      </Canvas>
+    </TouchableOpacity>
   );
 }
