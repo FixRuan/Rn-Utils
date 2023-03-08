8e73594c389decbd67984d604a911ba64380e77a
fixruan <ruangoio01@gmail.com>
2023-03-07 21:18:42 -0300

    display notification function

--- Notifee/App.tsx
+++ Notifee/App.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import notifee, { AndroidImportance } from "@notifee/react-native";
 
 export default function App() {
+  async function displayNotification() {
+    await notifee.requestPermission();
+
+    const channelId = await notifee.createChannel({
+      id: 'test',
+      name: 'sales',
+      vibration: true,
+      importance: AndroidImportance.HIGH
+    });
+
+    await notifee.displayNotification({
+      id: '1',
+      title: 'notification',
+      body: 'notification 1',
+      android: { channelId }
+    })
+  }
+
   return (
     <View>
       <Text>Notifee</Text>
+
+      <Button title="Enviar Notificação" onPress={displayNotification} />
     </View>
   );
 }
